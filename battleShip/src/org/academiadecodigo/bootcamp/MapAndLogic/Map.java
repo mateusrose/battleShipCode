981cfd2a9c976bbc647d4aabeb3ab20b6b352573
RubenAlmeidaSantos <rubenfe2333@gmail.com>
2024-03-01 15:34:36 +0000

    map work in progress

---
+++ battleShip/src/org/academiadecodigo/bootcamp/MapAndLogic/Map.java
@@ -0,0 +1,27 @@
+package org.academiadecodigo.bootcamp.MapAndLogic;
+
+public class Map {
+    private boolean[] X = new boolean[10];
+    private boolean[] Y = new boolean[10];
+
+    public boolean[] getX() {
+        return X;
+    }
+
+    public void setX(boolean[] x) {
+        X = x;
+    }
+
+    public boolean[] getY() {
+        return Y;
+    }
+
+    public void setY(boolean[] y) {
+        Y = y;
+    }
+    public Map(){
+        for (int i = 0; i < X.length ; i++) {
+            x
+        }
+    }
+}
