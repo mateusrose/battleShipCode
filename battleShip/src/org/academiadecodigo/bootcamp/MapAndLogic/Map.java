54b403f40fee04b1f7fa36b3fb898ce7347fe5a4
RubenSantos <rubenfe2333@gmail.com>
2024-03-02 13:29:34 +0000

    made some progress with the grid

--- battleShip/src/org/academiadecodigo/bootcamp/MapAndLogic/Map.java
+++ battleShip/src/org/academiadecodigo/bootcamp/MapAndLogic/Map.java
@@ -1,34 +1,102 @@
 package org.academiadecodigo.bootcamp.MapAndLogic;
 
-public class Map {
-    private boolean[][] position = new boolean[10][10];
+import java.util.Scanner;
 
-    public boolean[][] getPos() {
-        return position;
+public class Map {
+    private int numRows = 10;
+    private int numCols = 10;
+    private int playerShips;
+
+    private boolean isTurn;
+    private String[][] grid = new String[numRows][numCols];
+    private int[][] missedguesses = new int[numRows][numCols];
+
+    public String[][] getPos() {
+        return grid;
     }
 
-    public void setPosition(boolean[][] x) {
-        position = x;
+    public void setGrid(String[][] x) {
+        grid = x;
     }
     public Map(){
-        for (int i = 0; i < position.length ; i++) {
-            for (int j = 0; j < position.length; j++) {
-                position[i][j]= false;
+        System.out.print("  ");
+        for(int i = 0; i < numCols; i++)
+            System.out.print(i);
+        System.out.println();
+
+        //Middle section of Ocean Map
+        for(int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                grid[i][j] = " ";
+                if (j == 0)
+                    System.out.print(i + "|" + grid[i][j]);
+                else if (j == grid[i].length - 1)
+                    System.out.print(grid[i][j] + "|" + i);
+                else
+                    System.out.print(grid[i][j]);
+            }
+            System.out.println();
+        }
+
+        //Last section of Ocean Map
+        System.out.print("  ");
+        for(int i = 0; i < numCols; i++)
+            System.out.print(i);
+        System.out.println();
+    }
+
+    public void printOceanMap(){
+        System.out.println();
+        //First section of Ocean Map
+        System.out.print("  ");
+        for(int i = 0; i < numCols; i++)
+            System.out.print(i);
+        System.out.println();
+
+        //Middle section of Ocean Map
+        for(int x = 0; x < grid.length; x++) {
+            System.out.print(x + "|");
+
+            for (int y = 0; y < grid[x].length; y++){
+                System.out.print(grid[x][y]);
             }
 
-            }
+            System.out.println("|" + x);
         }
-    public void getArray(){
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                System.out.println("array "+position[i][j]);
-            }
 
+        //Last section of Ocean Map
+        System.out.print("  ");
+        for(int i = 0; i < numCols; i++)
+            System.out.print(i);
+        System.out.println();
+    }
+    public void deployPlayerShips(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("\nDeploy your ships:");
+        //Deploying five ships for player
+        playerShips = 5;
+        for (int i = 1; i <= playerShips; ) {
+            System.out.print("Enter X coordinate for your " + i + " ship: ");
+            int x = input.nextInt();
+            System.out.print("Enter Y coordinate for your " + i + " ship: ");
+            int y = input.nextInt();
+
+            if((x >= 0 && x < numRows) && (y >= 0 && y < numCols) && (grid[x][y] == " "))
+            {
+                grid[x][y] =   "X";
+                i++;
+            }
+            else if((x >= 0 && x < numRows) && (y >= 0 && y < numCols) && grid[x][y] == "X")
+                System.out.println("You can't place two or more ships on the same location");
+            else if((x < 0 || x >= numRows) || (y < 0 || y >= numCols))
+                System.out.println("You can't place ships outside the " + numRows + " by " + numCols + " grid");
         }
+        printOceanMap();
     }
 
     public static void main(String[] args) {
         Map map = new Map();
-        map.getArray();
+        map.deployPlayerShips();
     }
 }
